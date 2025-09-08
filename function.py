dcbad8c43a046aed0a351f04c4220714b847fae6
Jeremy Drapeau <202269459@cegepsherbrooke.qc.ca>
2025-09-08 09:37:29 -0400

    Documentation ajoute

--- function.py
+++ function.py
@@ -1,7 +1,9 @@
+# Demande un message a l'utilisateur
 def askMsg() -> str : 
     temp = input("Quel est le message a afficher? : ")
     return temp
 
+# Demande un chiffre a l'utilisateur
 def askNum() -> int :
     temp : int = int(input("Combien de fois voullez-vous l\'afficher"))
     return temp
